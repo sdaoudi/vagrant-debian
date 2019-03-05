eda2cfe639815e7577bebdf3a685ff40b659146e
DAOUDI Soufian <soufian.daoudi2@gmail.com>
2019-03-05 23:32:18 +0100

    Fixed portainer and mariadb data volumes

--- config/scripts/init.sh
+++ config/scripts/init.sh
@@ -9,7 +9,7 @@ docker run -d \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /vagrant/config/traefik/traefik.toml:/traefik.toml \
   -p 80:80 \
-	--restart always \
+  --restart always \
   -l traefik.frontend.rule=Host:traefik.local \
   -l traefik.port=8181 \
   --network bridge \
@@ -17,19 +17,27 @@ docker run -d \
   traefik:1.7.2-alpine
 
 # run portainer container
-docker volume create portainerdata
+PORTAINER_DIR="/home/vagrant/data/portainer"
+if [ ! -d "$PORTAINER_DIR" ]; then
+    mkdir -p $PORTAINER_DIR
+fi
 docker run -d \
 	-p 9005:9000 \
 	--restart always \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-	-v portainerdata:/data \
+        -v ${PORTAINER_DIR}:/data \
 	--name portainer \
 	portainer/portainer
 
 # run mariadb database container
+MYSQL_DIR="/home/vagrant/data/mysql"
+if [ ! -d "$MYSQL_DIR" ]; then
+    mkdir -p $MYSQL_DIR
+fi
 docker run -d \
 	-p 3306:3306 \
 	--restart always \
+        -v ${MYSQL_DIR}:/var/lib/mysql \
 	-e MYSQL_ROOT_PASSWORD=pouf \
 	--network bridge \
 	--name mariadb \
